a8fd8b0b25cc9d170c03cae5ac8a1f2904a83769
ItzDevX <48414176+ItzDevX@users.noreply.github.com>
2020-04-20 23:58:30 +0200

    Update Home.page.tsx

--- public/pages/Home/Home.page.tsx
+++ public/pages/Home/Home.page.tsx
@@ -5,6 +5,7 @@ import { Post, Tag, PostStatus } from "@fider/models";
 import { MultiLineText, Hint } from "@fider/components";
 import { SimilarPosts } from "./components/SimilarPosts";
 import { FaRegLightbulb } from "react-icons/fa";
+import { PostInput } from "./components/PostInput";
 import { PostsContainer } from "./components/PostsContainer";
 import { useFider } from "@fider/hooks";
 
@@ -27,31 +28,23 @@ const Lonely = () => {
         permanentCloseKey="at-least-3-posts"
         condition={fider.session.isAuthenticated && fider.session.user.isAdministrator}
       >
-        It's recommended that you wait <strong>at least 3</strong> hours before sharing this site.
+        It's recommended that you post <strong>at least 3</strong> suggestions here before sharing this site. The
         initial content is key to start the interactions with your audience.
-      </Hint>
-      <Hint>
-        I'm currently updating the application website. Report any bugs to @Nev_ermind. 
-        Everything will still be functional but some things might be weird.
       </Hint>
       <p>
         <FaRegLightbulb />
       </p>
-      <p>No new applications :c</p>
+      <p>It's lonely out here. Start by sharing a suggestion!</p>
     </div>
   );
 };
 
-
-const defaultWelcomeMessage = `Welcome to **BristoBakery Application** web interface. Here all applications from the application centre 
-will be listed. Enjoy reading! Found a bug or a problem? Contact **@Nev_ermind**
-
-Our server & API are getting a update.
-`;
-
+const defaultWelcomeMessage = `We'd love to hear what you're thinking about. 
+What can we do better? This is the place for you to vote, discuss and share ideas.`;
 
 const HomePage = (props: HomePageProps) => {
-  const [title] = useState("");
+  const fider = useFider();
+  const [title, setTitle] = useState("");
 
   const isLonely = () => {
     const len = Object.keys(props.countPerStatus).length;
@@ -69,12 +62,17 @@ const HomePage = (props: HomePageProps) => {
   return (
     <div id="p-home" className="page container">
       <div className="row">
-       <div className="l-welcome-col col-md-4">
+        <div className="l-welcome-col col-md-4">
           <MultiLineText
             className="welcome-message"
-            text={defaultWelcomeMessage}
+            text={fider.session.tenant.welcomeMessage || defaultWelcomeMessage}
             style="full"
-         </div>
+          />
+          <PostInput
+            placeholder={fider.session.tenant.invitation || "Enter your suggestion here..."}
+            onTitleChanged={setTitle}
+          />
+        </div>
         <div className="l-posts-col col-md-8">
           {isLonely() ? (
             <Lonely />
